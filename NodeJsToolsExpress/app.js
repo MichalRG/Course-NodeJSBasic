8f58378ed2d01e7874d27e6d874c6221d9786a3d
Michal <krzyzowskim1998@gmail.com>
2023-09-16 15:11:57 +0200

    Added two middlewares to process logs

--- NodeJsToolsExpress/app.js
+++ NodeJsToolsExpress/app.js
@@ -3,6 +3,17 @@
 
  const app = express(); // it exports function to reun server
 
- const server = http.createServer(app); // we pass function which is handler to running server
+ // it will executing evry time request will come, the method expect function as an argument
+ // u have to pass function with 3 parameters always
+ app.use((req, res, next) => {
+    console.log('test');
+    next(); // it allows to move to another middleware without it is returining response and finish middlewaring flow
+ }); //adding new middleware fucntion 
+
+ app.use((req,res,next)=> {
+    console.log('Another middleware!');
+ });
+
+ const server = http.createServer(app); // we pass function which is handler to running server  
 
  server.listen(3000); // keep process running for incoming request
