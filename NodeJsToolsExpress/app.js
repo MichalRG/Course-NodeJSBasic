832fb30d85e02e1a0c132a1e645a193717439333
Michal <krzyzowskim1998@gmail.com>
2023-09-16 16:29:59 +0200

    Added routing with express

--- NodeJsToolsExpress/app.js
+++ NodeJsToolsExpress/app.js
@@ -3,18 +3,20 @@
 
  const app = express(); // it exports function to reun server
 
- // it will executing evry time request will come, the method expect function as an argument
- // u have to pass function with 3 parameters always
- app.use((req, res, next) => {
-    console.log('test');
-    next(); // it allows to move to another middleware without it is returining response and finish middlewaring flow
- }); //adding new middleware fucntion 
+//  // it will executing evry time request will come, the method expect function as an argument
+//  // u have to pass function with 3 parameters always
+//  app.use((req, res, next) => {
+//     console.log('test');
+//     next(); // it allows to move to another middleware without it is returining response and finish middlewaring flow
+//  }); //adding new middleware fucntion 
 
- app.use((req,res,next)=> {
+app.use('/add-product', (req, res, next) => { // it has to be before the '/' becuase path '/add-product' also match to the bottom one, but because we do not call next it won't be handle
+    res.send('<h1>The Add Product Page</h1>');
+});
+
+ app.use('/', (req,res,next)=> { //default value for path is '/' (first argument), evrything what starts with slesh will be porcessed through this use call
     console.log('Another middleware!');
-    res.send('<h1>Hello from express! </h1>'); // default response haader has type /text/html
+    res.send('<h1>Hello from express! </h1>'); // default response haader has type /text/html if return text!
  });
 
- const server = http.createServer(app); // we pass function which is handler to running server  
-
- server.listen(3000); // keep process running for incoming request
+ app.listen(3000); //it creates server and call listen on pointed port (so the same code as previous)
