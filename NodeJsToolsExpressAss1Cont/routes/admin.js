93a1c7346fd6ba684852b3e7e58ca84e42c1d4a8
Michal <krzyzowskim1998@gmail.com>
2023-09-17 14:18:32 +0200

    Added separte html files and handling it as responses from paths

--- NodeJsToolsExpressAss1Cont/routes/admin.js
+++ NodeJsToolsExpressAss1Cont/routes/admin.js
@@ -1,9 +1,13 @@
+const path = require('path'); //core js module not needs to install
+
 const express = require('express');
 
 const router = express.Router();
  
 router.get('/add-product', (req,res,next) => {
-    res.send('<form action="add-product" method="POST"><input type="text" name="title"><button type="submit">OK</button></form>');
+    // res.send('<form action="add-product" method="POST"><input type="text" name="title"><button type="submit">OK</button></form>');
+
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 //app.use is universal 
--- NodeJsToolsExpressAss1Cont/routes/shop.js
+++ NodeJsToolsExpressAss1Cont/routes/shop.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req,res,next) => {
-    res.send('<h1>Yo in multiverse!</h1>');
+    //path join will build automaticaly path with /, using __dirname it return absolute path to the current location in your system (shop.js that's why we need after it to go back ../)
+    res.sendFile(path.join(__dirname, '../', 'views','shop.html')); 
+
+    // res.send('<h1>Yo in multiverse!</h1>');
 });
 
 module.exports = router;
