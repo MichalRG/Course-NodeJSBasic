d7001dd5621c8ed41edf24dcb41c899ac047d20b
Michal <krzyzowskim1998@gmail.com>
2023-09-27 17:36:08 +0200

    Changed hbs into ejs template rendering format

--- NodeJsDynamicContent_2709/app.js
+++ NodeJsDynamicContent_2709/app.js
@@ -7,10 +7,11 @@ const expressHbs = require('express-handlebars');
 const app = express();
 
 //registering new template engines not build in express (pug was already builded in )
-app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'handlebars'})); // we have to tell express js that we installed such handlebars template enginge
+// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'handlebars'})); // we have to tell express js that we installed such handlebars template enginge
 // to point layout folder we hasve to pass optional argument to expressHbs with configuration
 // the default value for arguments are as set above (so we can skip it not like in the exampel above).
-app.set('view engine', 'handlebars');
+// app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 // app.set('view engine', 'pug'); //setting global values which nodejs understand
 //view engine is one pre-defined value (same as views)
 // app.set('views', 'views'); //vies it's default that why this confiugration value we didnt set earlier 
