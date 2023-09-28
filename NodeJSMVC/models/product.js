db6392d631d0e22134b4ef67dd030b98d23a8813
Michal <krzyzowskim1998@gmail.com>
2023-09-28 12:05:09 +0200

    Added manging data via model with using reading/ saving data into file

--- NodeJSMVC/models/product.js
+++ NodeJSMVC/models/product.js
@@ -1,15 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
 
-module.exports =  class Product {
+module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
-    save () {
-        products.push(this);
+    save() {
+        const pathToManage = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'product.json'
+        );
+        
+        fs.readFile(pathToManage, (err, fileContent) => {
+            let products = [];
+
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+
+            products.push(this); 
+
+            fs.writeFile(pathToManage, JSON.stringify(products), (err) => {
+                console.error(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products; 
+    static fetchAll(cb) {
+        const pathToRead = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'product.json'
+        );
+
+        fs. readFile(pathToRead, (err, fileContent) => {
+            if (err || !fileContent) {
+                return cb([]);
+            }
+
+            cb(JSON.parse(fileContent));
+        });
     }
 }
