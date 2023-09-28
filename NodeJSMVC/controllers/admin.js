aeba88c619dae1c1e41902cd486451706e51a812
Michal <krzyzowskim1998@gmail.com>
2023-09-28 14:15:47 +0200

    Extended form, added new form controls like pirce, description, imageurl

--- NodeJSMVC/controllers/admin.js
+++ NodeJSMVC/controllers/admin.js
@@ -11,19 +11,23 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, description, price);
 
     product.save();
 
     res.redirect('/');
 };
 
-exports.getProducts = (req,res,next) => {
+exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('admin/product-list', {
-          prods: products,
-          pageTitle: 'Admin Products',
-          path: '/admin/products'
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
         });
     });
 }
