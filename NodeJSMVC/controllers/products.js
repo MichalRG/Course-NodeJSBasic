6e9c77d0e2b2dc74ac5ec8e837bed6feb3d63eac
Michal <krzyzowskim1998@gmail.com>
2023-09-28 13:46:31 +0200

    Restructured the archtiecture of catalogs

--- NodeJSMVC/controllers/products.js
+++ NodeJSMVC/controllers/products.js
@@ -1,8 +1,7 @@
 const Product = require('../models/product');
 
-
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
@@ -21,7 +20,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-        res.render('shop', {
+        res.render('shop/product-list', {
           prods: products,
           pageTitle: 'Shop',
           path: '/',
