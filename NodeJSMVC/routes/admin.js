45c25e393abbff83c3233ed2518a1bd64b3c2ec5
Michal <krzyzowskim1998@gmail.com>
2023-09-28 11:33:59 +0200

    Started new part with rebuilding architecture into MVC model, added first controllers for products and moved logic from routes to controllers

---
+++ NodeJSMVC/routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
+
+exports.routes = router;
+
