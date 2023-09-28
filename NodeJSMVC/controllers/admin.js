30bb8ae95c72dce0d6d7c66418282bfd929c190f
Michal <krzyzowskim1998@gmail.com>
2023-09-28 14:09:08 +0200

    added routes, added .ejs files for new routes, adjusted controllers to the new routes

--- NodeJSMVC/controllers/admin.js
+++ NodeJSMVC/controllers/admin.js
@@ -18,15 +18,12 @@ exports.postAddProduct = (req, res, next) => {
     res.redirect('/');
 };
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = (req,res,next) => {
     Product.fetchAll((products) => {
-        res.render('shop/product-list', {
+        res.render('admin/product-list', {
           prods: products,
-          pageTitle: 'Shop',
-          path: '/',
-          hasProducts: products.length > 0,
-          activeShop: true,
-          productCSS: true
+          pageTitle: 'Admin Products',
+          path: '/admin/products'
         });
     });
-};
+}
